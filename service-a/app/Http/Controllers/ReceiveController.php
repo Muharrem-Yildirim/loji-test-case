04021f9c489aa1015dcd2e58ef993d4a2d82950f
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-12-08 12:18:43 +0300

    service-a receiver response json schema changed

--- service-a/app/Http/Controllers/ReceiveController.php
+++ service-a/app/Http/Controllers/ReceiveController.php
@@ -8,6 +8,14 @@ class ReceiveController extends Controller
 {
     public function store(Request $request)
     {
-        return response()->json($request->all());
+        return response()->json([
+            'success' => true,
+            'message' => sprintf(
+                'Your message has been received by <b>%s</b> with following data: <b>%s</b> <br/><br/> X-Trace-ID: %s',
+                'service-a',
+                json_encode($request->all()),
+                $request->header('X-Trace-ID')
+            ),
+        ]);
     }
 }
