d5392611b737d2ccec31272855e6e5650636117c
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-12-08 21:53:04 +0300

    metric enums created

---
+++ service-b/app/Enums/MetricNames.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum MetricNames: string
+{
+    case MESSAGES_PROCESSED_TOTAL = 'messages_processed_total';
+}
---
+++ service-b/app/Enums/MetricNamespaces.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum MetricNamespaces: string
+{
+    case REQUEST = 'request';
+}
