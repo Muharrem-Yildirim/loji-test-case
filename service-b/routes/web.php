1e9f62f57af564613f98ccb727281231683ff10d
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-12-08 21:13:25 +0300

    metrics route added to service-b

--- service-b/routes/web.php
+++ service-b/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response('Welcome to Service-B.', 200);
 });
+
+
+Route::resource('metrics', \App\Http\Controllers\MetricsController::class)->only('index');
