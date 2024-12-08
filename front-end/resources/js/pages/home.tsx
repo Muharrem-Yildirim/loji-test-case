24362d5d296bd01edb257bda0d71959fd84bd391
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-12-08 12:33:45 +0300

    added message input to send request component, button is disabling when in request

--- front-end/resources/js/pages/home.tsx
+++ front-end/resources/js/pages/home.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { toast } from "@/hooks/use-toast";
 import { PageProps } from "@/types";
 import { Head, router } from "@inertiajs/react";
+import { Loader2 } from "lucide-react";
 import { useState } from "react";
 
 export default function Home({
@@ -27,10 +29,17 @@ export default function Home({
 }
 
 const MakeRequest = ({ traceId = null }: { traceId?: string | null }) => {
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState("");
+
     const sendRequest = () => {
+        setLoading(true);
+
         router.post(
             route("home.store"),
-            {},
+            {
+                message: message,
+            },
             {
                 onSuccess: ({ props }) => {
                     console.log(props);
@@ -54,6 +63,9 @@ const MakeRequest = ({ traceId = null }: { traceId?: string | null }) => {
                         variant: "destructive",
                     });
                 },
+                onFinish: () => {
+                    setLoading(false);
+                },
             }
         );
     };
@@ -66,8 +78,19 @@ const MakeRequest = ({ traceId = null }: { traceId?: string | null }) => {
                 </h2>
             </div>
             {traceId && <p>Trace ID: {traceId}</p>}
-            <Button className="mt-10" onClick={sendRequest}>
-                Send
+            <Input
+                className="mt-10 w-1/3"
+                placeholder="Message"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button className="mt-10" onClick={sendRequest} disabled={loading}>
+                {loading ? (
+                    <Loader2 className="animate-spin" />
+                ) : (
+                    "Send Request"
+                )}
             </Button>
         </div>
     );
