416d11e61e795b1e53bbd9c9cad6e41188ba5946
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-12-08 12:34:31 +0300

    helper file created, context log params added for service-a

--- service-a/app/Http/Controllers/ReceiveController.php
+++ service-a/app/Http/Controllers/ReceiveController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ReceiveController extends Controller
 {
     public function store(Request $request)
     {
+        Log::channel('frontend.receive')->info(json_encode($request->all()));
+
+        //@TODO: send to service-b via RabbitMQ publish
+
         return response()->json([
             'success' => true,
             'message' => sprintf(
