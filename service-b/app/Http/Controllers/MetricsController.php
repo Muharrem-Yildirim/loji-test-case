d3df10fcec725d8a6a577c3e19f5c19bb338173b
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-12-08 21:54:54 +0300

    removed unused imports

--- service-b/app/Http/Controllers/MetricsController.php
+++ service-b/app/Http/Controllers/MetricsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Services\PrometheusService;
-use Illuminate\Support\Facades\Log;
 
 class MetricsController extends Controller
 {
--- service-b/routes/web.php
+++ service-b/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Services\PrometheusExporter;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
