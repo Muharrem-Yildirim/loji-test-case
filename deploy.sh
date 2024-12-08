0f5b972ea0796b4c2c689de6e4e416ec76881b98
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-12-08 22:45:38 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,30 +4,29 @@ set -e
 
 echo "Building and starting Docker containers..."
 
-echo "Copying .env.example to .env and generating application key..."
+docker-compose down
+docker-compose build --no-cache
+docker-compose up -d
 
 cd front-end
+
 cp .env.example .env
 php artisan key:generate
 
 cd ../service-a
+
 cp .env.example .env
 php artisan key:generate
+
 
 cd ../service-b
+
 cp .env.example .env
 php artisan key:generate
 
-docker-compose build
-docker-compose up -d
 
 echo "Waiting for containers to fully initialize..."
-sleep 10  
 
-echo "Running migrations in front-end container"
-docker exec loji-test-case-front_end-1 php artisan migrate --force
-
-echo "Front-end deployment completed successfully!"
 
 echo "Running migrations in service-a container"
 docker exec loji-test-case-service_a-1 php artisan migrate --force
@@ -38,3 +37,10 @@ echo "Running migrations in service-b container"
 docker exec loji-test-case-service_b-1 php artisan migrate --force
 
 echo "Service-B deployment completed successfully!"
+
+echo "Running migrations in front-end container"
+
+docker exec loji-test-case-front_end-1 php artisan migrate
+
+echo "Front-end deployment completed successfully!"
+
